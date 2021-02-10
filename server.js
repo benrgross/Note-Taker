7a300b183795af26b3f233ed81401b5578467ff5
Ben Gross <benrgross@gmail.com>
2021-02-10 18:02:06 -0500

    added server ajax page get.

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+//set up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Send user to the ajax page
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "notes.html")));
 
 app.get("/", (req, res) => {
   res.send("the server is listening");
